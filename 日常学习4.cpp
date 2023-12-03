0a21d8ecb4534aebe3aa6dc6996c541e603eccc4
Yiyi <152374896+yiyi666663@users.noreply.github.com>
2023-12-03 20:43:04 +0800

    Add files via upload

---
+++ 日常学习4.cpp
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)
+//			printf("333\n");
+//		printf("666\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for(i = 0; i < 10; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	for (;;)
+//		printf("hehe\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//	
+//		for (; j < 10; j++)
+//		{
+//			printf("ÎÒ°®Äã  ");
+//		}
+//	}
+//	return 0;
+//}
+int main()
+{
+	int x, y;
+	for (x = 0, y = 0; x < 2 && y<5; ++x, y++)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}
