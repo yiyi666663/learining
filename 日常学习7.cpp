5fbb34dbd34008eae3ba2befe2c9830fb21eb58d
Yiyi <152374896+yiyi666663@users.noreply.github.com>
2023-12-08 17:46:29 +0800

    Add files via upload

---
+++ 日常学习7.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	char arr1[] = "welcome to bit!!!!";
+//	char arr2[] = "##################";
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	int left =0;
+//	int right = sz - 2;
+//	//int righe = strlen(arr1) - 1;
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		printf("left= %d\n", left);
+//		printf("right= %d\n", right);
+//		left++;
+//		right--;
+//		
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "welcome to bit!!!!";
+//	int left = 0;
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	printf("%d", sz);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	while (a = 3)
+//		printf("hehe");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("ÇëÊäÈëÃÜÂë: ");
+//		scanf("%s", &password);
+//		if (strcmp(password,"123456") ==0)
+//		{
+//			printf("µÇÂ½³É¹¦\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("ÃÜÂë´íÎó£¬ÇëÖØÊÔ\n");
+//		}
+//	}
+//	if (i == 3)
+//		printf("Èý´ÎÃÜÂë¾ù´íÎó£¬ÇëÍË³öµÇÂ¼");
+//	return 0;
+//}
